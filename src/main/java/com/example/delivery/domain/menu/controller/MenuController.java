30fa76bef5dad8c0f0d09cc51f188108405e23be
Park seong ho <hgg0305@daum.net>
2025-02-28 20:40:45 +0900

    feat : 기본적인 crud구현

---
+++ src/main/java/com/example/delivery/domain/menu/controller/MenuController.java
@@ -0,0 +1,62 @@
+package com.example.delivery.domain.menu.controller;
+
+import com.example.delivery.domain.menu.dto.requestDto.MenUpdateRequstDto;
+import com.example.delivery.domain.menu.dto.requestDto.MenuCreateRequestDto;
+import com.example.delivery.domain.menu.dto.responseDto.MenuCreateResponseDto;
+import com.example.delivery.domain.menu.dto.responseDto.MenuFindResponseDto;
+import com.example.delivery.domain.menu.dto.responseDto.MenuOrderResponseDto;
+import com.example.delivery.domain.menu.dto.responseDto.MenuUpdateResponseDto;
+import com.example.delivery.domain.menu.service.MenuService;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/menus")
+@RequiredArgsConstructor
+public class MenuController {
+  private final MenuService menuService;
+
+  // 메뉴 생성
+  @PostMapping("/stores/{storeId}")
+  public ResponseEntity<MenuCreateResponseDto> createMenu(@PathVariable Long storeId, @Valid @RequestBody MenuCreateRequestDto requestDto){
+    MenuCreateResponseDto menuCreateResponseDto = menuService.createMenu(storeId, requestDto.getMenuName(),requestDto.getPrice());
+
+    return new ResponseEntity<>(menuCreateResponseDto, HttpStatus.CREATED);
+
+  }
+
+  // 메뉴 수정
+  @PutMapping("/{menuId}")
+  public ResponseEntity<MenuUpdateResponseDto> updateMenu(@PathVariable Long menuId, @Valid@RequestBody MenUpdateRequstDto requstDto){
+    MenuUpdateResponseDto menuUpdateResponseDto = menuService.updateMenu(menuId, requstDto.getMenuName(), requstDto.getPrice());
+    return new ResponseEntity<>(menuUpdateResponseDto, HttpStatus.OK);
+  }
+
+  // 가게별 메뉴 조회
+  @GetMapping("/stores/{storeId}")
+  public ResponseEntity<List<MenuFindResponseDto>> getMenusByStore(@PathVariable Long storeId){
+    List<MenuFindResponseDto> menuList = menuService.getMenusByStore(storeId);
+    return new ResponseEntity<>(menuList,HttpStatus.OK);
+  }
+
+  // 주문 내역 메뉴 조회
+  @GetMapping("/orders/{orderId}")
+  public ResponseEntity<MenuOrderResponseDto> getMenusByOrder(@PathVariable Long orderId){
+    MenuOrderResponseDto menuOrderResponseDto = menuService.getMenusByOrder(orderId);
+    return new ResponseEntity<>(menuOrderResponseDto, HttpStatus.OK);
+  }
+
+  // 메뉴 삭제
+  @DeleteMapping("/{menuId}")
+  public ResponseEntity<String> deleteMenu(@PathVariable Long menuId) {
+    menuService.deleteMenu(menuId);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+}
--- src/main/java/com/example/delivery/domain/menu/entity/Menu.java
+++ src/main/java/com/example/delivery/domain/menu/entity/Menu.java
@@ -1,8 +1,10 @@
 package com.example.delivery.domain.menu.entity;
-
+import com.example.delivery.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -14,19 +16,33 @@ public class Menu {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String menuName;
-
   private Long price;
-
+  //가게와의 연관 관계
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name ="store_id", nullable = false)
   private Store store;
 
-  private LocalDateTime created_at;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+  private LocalDateTime deletedAt;
 
-  private LocalDateTime updated_at;
+  public Menu(String menuName, Long price,Store store){
+    this.menuName = menuName;
+    this.price = price;
+    this.store = store;
+  }
 
-  private LocalDateTime deleted_at;
+  // 소프트 딜리트 메서드
+  public void delete(){
+    this.deletedAt = LocalDateTime.now();
+  }
+  // 메뉴 수정 메서드
+  public void update(String menuName, Long price){
+    this.menuName = menuName;
+    this.price = price;
+  }
 
 }
