1276e66c218c71e3b35bca079b66777ed68edd4c
Park seong ho <hgg0305@daum.net>
2025-03-03 22:37:26 +0900

    fix : MenuRepository JPQL문 수정

--- src/main/java/com/example/delivery/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/delivery/domain/menu/repository/MenuRepository.java
@@ -10,9 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
-  @Query("SELECT m FROM Menu m WHERE m.store.id = :storeId AND m.deletedAt IS NULL")
+  // sotreId에 해당하는 Menu 목록 조회하는 쿼리(삭제된 메뉴는 포함 안함)
+  @Query("SELECT m FROM Menu m JOIN FETCH m.store WHERE m.store.id = :storeId AND m.deletedAt IS NULL")
   List<Menu> findMenusByStoreId(@Param("storeId") Long storeId);
+
 
 
 }
