51e0719c304dc6b30f43b753c239ba0a8a67ca36
Park seong ho <hgg0305@daum.net>
2025-03-04 17:49:20 +0900

    fix : 변경사항 커밋

--- src/main/java/com/example/delivery/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/delivery/domain/order/repository/OrderRepository.java
@@ -5,8 +5,7 @@ import com.example.delivery.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.Optional;
 
