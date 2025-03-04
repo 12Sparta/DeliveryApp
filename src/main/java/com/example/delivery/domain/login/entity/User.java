ecb633300dead11bff35ffcdfa522b233917e122
Park seong ho <hgg0305@daum.net>
2025-03-04 18:19:40 +0900

    fix : user entity ID long -> Long

--- src/main/java/com/example/delivery/domain/login/entity/User.java
+++ src/main/java/com/example/delivery/domain/login/entity/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 
     @Enumerated(EnumType.STRING)
